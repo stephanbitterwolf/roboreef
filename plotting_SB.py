ec0d5b8b3b3e304ae3fcc0aef7c7771bb6d08d8c
KYLE RASMUSSEN <kylerasmussen@inventati.org>
2018-08-11 12:37:11 -0700

    reformatting and taking out broken code

--- plotting_SB.py
+++ plotting_SB.py
@@ -6,6 +6,8 @@ from datetime import datetime
 fig = plt.figure()
 rect = fig.patch
 rect.set_facecolor('#0079E7')
+
+
 def animate(i):
     ftemp = 'temp.csv'
     fh = open(ftemp)
@@ -14,32 +16,33 @@ def animate(i):
     for line in fh:
         pieces = line.split(',')
         degree = pieces[0]
-        timeB=  pieces[1]
-        timeA= timeB[:8]
-            #print timeA
-        time_string = datetime.strptime(timeA,'%H:%M:%S')
-        #print time_string
+        timeB = pieces[1]
+        timeA = timeB[:8]
+        # print timeA
+        time_string = datetime.strptime(timeA, '%H:%M:%S')
+        # print time_string
         try:
             temp.append(float(degree))
             timeC.append(time_string)
         except:
-            print ("dont know")
-        
+            print("dont know")
 
-        ax1 = fig.add_subplot(1,1,1,axisbg='white')
+        ax1 = fig.add_subplot(1, 1, 1, axisbg='white')
         ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
         ax1.clear()
-        ax1.plot(timeC,temp, 'c', linewidth = 3.3)
+        ax1.plot(timeC, temp, 'c', linewidth=3.3)
         plt.title('Temperature')
         plt.xlabel('Time')
-        
-ani = animation.FuncAnimation(fig, animate, interval = 6000)   
+
+
+ani = animation.FuncAnimation(fig, animate, interval=6000)
 plt.show()
 
-
+# I don't know what this does.
+'''
 */
 
 void setup() {
     
 }
-
+'''
